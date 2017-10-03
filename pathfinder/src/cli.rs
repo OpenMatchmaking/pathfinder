e827a552eafaf54cf817cbdf66472a4fa8fc3a64
Valeryi Savich <Relrin78@gmail.com>
2017-10-03 16:14:29 +0300

    Updated methods for work with clap-rs

--- pathfinder/src/cli.rs
+++ pathfinder/src/cli.rs
@@ -1,33 +1,27 @@
 extern crate clap;
 
+use std::cell::RefCell;
 use self::clap::{App, ArgMatches};
 
 
-pub struct Cli<'a: 'b, 'b> {
-    app: App<'a, 'b>
+pub fn cli<'a>() -> ArgMatches<'a> {
+    App::new("Pathfinder")
+        .version("0.1.0")
+        .author("Valeryi Savich <relrin78@gmail.com>")
+        .about("WebSocket-over-Kafka reverse proxy")
+        .args_from_usage(
+           "-c, --config=[FILE]       'Path to a custom settings file'
+            -i, --ip=[IP]             'The used IP for a server'
+            -p, --port=[PORT]         'The listened port'
+            -C, --cert=[CERTIFICATE]  'Path to a used certificate'
+            -K, --key=[KEY]           'Path to a public key'",
+        )
+        .get_matches()
 }
 
 
-impl<'a: 'b, 'b> Cli<'a, 'b>{
-    pub fn new() -> Cli<'a, 'b> {
-        Cli {
-            app: App::new("Reverse proxy")
-                .version("0.1.0")
-                .author("Valeryi Savich <relrin78@gmail.com>")
-                .about("WebSocket-over-Kafka reverse proxy")
-                .args_from_usage(
-                    "-c, --config=[FILE]      'Path to a custom settings file'
-                    -i, --ip=[IP]             'The used IP for a server'
-                    -p, --port=[PORT]         'The listened port'
-                    -C, --cert=[CERTIFICATE]  'Path to a used certificate'
-                    -K, --key=[KEY]           'Path to a public key'"
-                )
-        }
-    }
-
-    pub fn get_value(self, key: &'a str, default: &'a str) -> &'a str {
-        let matches = self.app.get_matches();
-        matches.value_of(key).unwrap_or(default)
-    }
+pub fn get_value<'a>(cli: &'a ArgMatches<'a>, key: &'a str, default: &'a str) -> &'a str {
+    let cli = RefCell::new(cli);
+    let cli = cli.into_inner();
+    cli.value_of(key).unwrap_or(default)
 }
-
