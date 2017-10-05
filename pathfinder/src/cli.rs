2b141e7c45f1b7625aa6b728c6323fe1f005f1c9
Valeryi Savich <Relrin78@gmail.com>
2017-10-05 22:15:22 +0300

    Added tests for get_value function

--- pathfinder/src/cli.rs
+++ pathfinder/src/cli.rs
@@ -25,3 +25,29 @@ pub fn get_value<'a>(cli: &'a ArgMatches<'a>, key: &'a str, default: &'a str) ->
     let cli = cli.into_inner();
     cli.value_of(key).unwrap_or(default)
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::clap::{App, Arg};
+    use super::{cli, get_value};
+
+    #[test]
+    fn test_get_value_returns_extracted_value() {
+        let cli = App::new("Test CLI")
+                    .version("0.1.0")
+                    .arg(Arg::with_name("config")
+                        .short("c")
+                        .long("config")
+                        .default_value("test")
+                    )
+                   .get_matches();
+        assert_eq!(get_value(&cli, "config", "not found"), "test");
+    }
+
+    #[test]
+    fn test_get_value_returns_default_value() {
+        let cli = cli();
+        assert_eq!(get_value(&cli, "WRONG_KEY", "not found"), "not found");
+    }
+}
