6d7d0434be86cb8cbfd297907fc57c45d8c6910f
Valeryi Savich <Relrin78@gmail.com>
2017-10-15 17:54:51 +0300

    Implemented tranforming URLs to the Kafka topics

--- pathfinder/src/proxy.rs
+++ pathfinder/src/proxy.rs
@@ -32,7 +32,7 @@ impl Proxy {
         println!("Listening on: {}", address);
 
         // The server loop.
-        let server = socket.incoming().for_each(|(stream, addr)| {
+        let server = socket.incoming().for_each(|(stream, _addr)| {
             let proxy_inner = self;
 
             // Handler per each connection.
@@ -48,14 +48,17 @@ impl Proxy {
                 })
                 // Process the messages
                 .and_then(move |ws_stream| {
+                    let (_sink, stream) = ws_stream.split();
+
                     // Per each message
-                    ws_stream.for_each(move |message: Message| {
+                    stream.for_each(move |message: Message| {
                         // Convert an incoming message into JSON
                         let text_message = try!(message.into_text());
-                        let json_message = try!(proxy_inner.decode_message(text_message.as_str()));
+                        let parsed_json = try!(proxy_inner.decode_message(text_message.as_str()));
 
                         // Convert the specified API url to a Kafka topic
-                        println!("Message = {}", json_message);
+                        let json_message = try!(proxy_inner.validate_json(parsed_json));
+                        let microservice = proxy_inner.match_microservice(json_message);
                         Ok(())
                     })
                     .map_err(|err| {
@@ -69,10 +72,36 @@ impl Proxy {
         core.run(server).unwrap();
     }
 
-    fn decode_message(&self, message: &str) -> BaseResult<JsonValue, Error> {
+    fn decode_message(&self, message: &str) -> BaseResult<Box<JsonValue>, Error> {
         match parse_json(message) {
-            Ok(message) => Ok(message),
+            Ok(message) => Ok(Box::new(message)),
             Err(err) => Err(Error::new(ErrorKind::InvalidData, err))
+        }
+    }
+
+    fn validate_json(&self, json: Box<JsonValue>) -> BaseResult<Box<JsonValue>, Error> {
+        if !json.has_key("url") {
+            return Err(Error::new(ErrorKind::Other, "Key `url` is missing or value is `null`"));
+        }
+
+        if json.has_key("matchmaking") {
+            return Err(Error::new(ErrorKind::Other, "Key `matchmaking` must be not specified"));
+        }
+
+        Ok(json)
+    }
+
+    fn match_microservice(&self, json: Box<JsonValue>) -> String {
+        let url = json["url"].as_str().unwrap();
+
+        match self.router.match_url(url) {
+            Ok(endpoint) => endpoint.get_microservice(),
+            _ => {
+                let mut external_url = url.clone();
+                external_url = external_url.trim_left_matches("/");
+                external_url = external_url.trim_right_matches("/");
+                external_url.replace("/", ".")
+            }
         }
     }
 }
