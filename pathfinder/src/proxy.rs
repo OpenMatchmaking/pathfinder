fe7cd5223c991fd0f695c4a860dcd53575f37416
Valeryi Savich <Relrin78@gmail.com>
2017-10-15 16:33:21 +0300

    Implemented parsing message into JSON objects

--- pathfinder/src/proxy.rs
+++ pathfinder/src/proxy.rs
@@ -1,16 +1,16 @@
 use std::io::{Error, ErrorKind};
 use std::net::SocketAddr;
+use std::result::{Result as BaseResult};
 
 use super::router::{Router};
 
 use futures::{Future, Sink};
 use futures::stream::{Stream};
-use json::{parse as parse_json};
+use json::{parse as parse_json, JsonValue};
 use tokio_core::net::{TcpListener};
 use tokio_core::reactor::{Core};
 use tokio_tungstenite::{accept_async};
 use tungstenite::protocol::{Message};
-
 
 
 pub struct Proxy {
@@ -33,7 +33,7 @@ impl Proxy {
 
         // The server loop.
         let server = socket.incoming().for_each(|(stream, addr)| {
-            let handle_inner = handle.clone();
+            let proxy_inner = self;
 
             // Handler per each connection.
             accept_async(stream)
@@ -46,22 +46,33 @@ impl Proxy {
                     println!("Checking the user's token");
                     Ok(ws_stream)
                 })
-                // Process the message
+                // Process the messages
                 .and_then(move |ws_stream| {
+                    // Per each message
                     ws_stream.for_each(move |message: Message| {
-                        // Convert message into text
-                        // After it try to parse into JSON
-                        // Convert specified API url to a Kafka topic
-                        println!("Message: {}", message);
+                        // Convert an incoming message into JSON
+                        let text_message = try!(message.into_text());
+                        let json_message = try!(proxy_inner.decode_message(text_message.as_str()));
+
+                        // Convert the specified API url to a Kafka topic
+                        println!("Message = {}", json_message);
                         Ok(())
-                    }).map_err(|err| {
+                    })
+                    .map_err(|err| {
                         println!("Occurred error during the processing a message: {}", err);
-                        Error::new(ErrorKind::InvalidData, err)
+                        Error::new(ErrorKind::Other, err)
                     })
                 })
         });
 
         // Run the server
         core.run(server).unwrap();
+    }
+
+    fn decode_message(&self, message: &str) -> BaseResult<JsonValue, Error> {
+        match parse_json(message) {
+            Ok(message) => Ok(message),
+            Err(err) => Err(Error::new(ErrorKind::InvalidData, err))
+        }
     }
 }
