35a58cc795d981dbebc61ecba96215f783b9e0ac
Valeryi Savich <Relrin78@gmail.com>
2017-11-28 13:48:27 +0300

    Implemented extracting token from a header

--- pathfinder/src/token/middleware.rs
+++ pathfinder/src/token/middleware.rs
@@ -21,17 +21,25 @@ impl JwtTokenMiddleware {
             jwt_secret: cli.jwt_secret_key.clone()
         }
     }
+
+    fn extract_token_from_header(&self, raw_token: &[u8]) -> Result<String> {
+        match str::from_utf8(raw_token) {
+            Ok(parsed_value) => Ok(String::from(parsed_value)),
+            Err(_) => {
+                let message = String::from("Token is invalid.");
+                Err(PathfinderError::AuthenticationError(message))
+            }
+        }
+    }
 }
 
 
 impl Middleware for JwtTokenMiddleware {
     fn process_request(&self, request: &Request) -> Result<Option<Vec<(String, String)>>> {
         match request.headers.find_first("Sec-WebSocket-Protocol") {
-             Some(token) => {
-                 let parsed_value = str::from_utf8(token).unwrap();
-                 let extra_headers = vec![
-                    (String::from("Sec-WebSocket-Protocol"), String::from(parsed_value))
-                 ];
+             Some(raw_token) => {
+                 let token = self.extract_token_from_header(raw_token)?;
+                 let extra_headers = vec![(String::from("Sec-WebSocket-Protocol"), token)];
                  Ok(Some(extra_headers))
              },
              None => {
