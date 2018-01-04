07ababf14abce79a577b885c87835fb4fb5c9dbd
Valeryi Savich <Relrin78@gmail.com>
2018-01-04 12:31:48 +0300

    Added documentation for auth/token/jwt.rs

--- pathfinder/src/auth/token/jwt.rs
+++ pathfinder/src/auth/token/jwt.rs
@@ -1,11 +1,15 @@
+//! Wrappers for validating a JSON Web Token
+//!
+
 use super::super::super::error::{Result, PathfinderError};
 
 use jsonwebtoken::{decode, TokenData, Validation};
 
-
+/// A default issuer in JSON Web Token
 pub const DEFAULT_ISSUER: &'static str = "pathfinder";
 
 
+/// A struct with validated fields in JSON Web Token
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
     pub iss: String,
@@ -13,6 +17,7 @@ pub struct Claims {
 }
 
 
+/// Validates a passed token with the certain secret key and a validation structure.
 pub fn validate(token: &str, secret_key: &str, validation_struct: &Validation) -> Result<TokenData<Claims>> {
     match decode::<Claims>(token, secret_key.as_bytes(), &validation_struct) {
         Ok(valid_token) => Ok(valid_token),
